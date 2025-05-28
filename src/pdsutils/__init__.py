9a1310623e6b539f15def1c414e8d12531906169
jjacoby <joshskisoccer@gmail.com>
2025-05-28 09:25:15 -0700

    Added property_checker to init

--- src/pdsutils/__init__.py
+++ src/pdsutils/__init__.py
@@ -1,3 +1,4 @@
 from .max_relative_motion import max_relative_motion
-__all__ = ["maximum_relative_motion"]
+from .property_checker import check_property
+__all__ = ["maximum_relative_motion", "check_property"]
 
