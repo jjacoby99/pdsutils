8c8e6fec65897e45c10b64095ab4b7a3cc9aa047
jjacoby <joshskisoccer@gmail.com>
2025-05-28 14:34:07 -0700

    updated init for new function

--- src/pdsutils/__init__.py
+++ src/pdsutils/__init__.py
@@ -1,4 +1,7 @@
-from .max_relative_motion import max_relative_motion
+from .max_relative_motion import maximum_relative_motion
 from .property_checker import check_property
-__all__ = ["maximum_relative_motion", "check_property"]
+from .model_setup import duplicate
+__all__ = ["maximum_relative_motion", 
+           "check_property", 
+           "duplicate"]
 
